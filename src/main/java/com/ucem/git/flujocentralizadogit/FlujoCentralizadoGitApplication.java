3fa82df909ae3a9be445e4829e15892ba571f65e
Felix Garcia <fm8910@gmail.com>
2023-03-17 14:49:25 -0600

    configuracion del proyecto

---
+++ src/main/java/com/ucem/git/flujocentralizadogit/FlujoCentralizadoGitApplication.java
@@ -0,0 +1,13 @@
+package com.ucem.git.flujocentralizadogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlujoCentralizadoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlujoCentralizadoGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucem/git/flujocentralizadogit/FlujoCentralizadoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucem.git.flujocentralizadogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlujoCentralizadoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
