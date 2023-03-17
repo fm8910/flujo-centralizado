c4d6660536f993916dce80af07d385fc313cd5c1
MayronBM <bmayronantonio@gmail.com>
2023-03-17 15:18:33 -0600

    cambia main

--- src/main/java/com/ucem/git/flujocentralizadogit/FlujoCentralizadoGitApplication.java
+++ src/main/java/com/ucem/git/flujocentralizadogit/FlujoCentralizadoGitApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlujoCentralizadoGitApplication {
 
+	//este es el main
 	public static void main(String[] args) {
-		SpringApplication.run(FlujoCentralizadoGitApplication.class, args);
+		SpringApplication
+				.run(FlujoCentralizadoGitApplication.class
+						, args);
 	}
 
 }
